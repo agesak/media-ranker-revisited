728751de7fdb65b01263ea43b96a712e8ecd16bf
Kareha Agesa <kmaagesa@gmail.com>
2020-11-27 14:43:10 -0800

    user controller/model methods for login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,23 @@ class UsersController < ApplicationController
 
   def create
     auth_hash = request.env["omniauth.hash"]
+
+    # raise
+    user = User.find_by(uid: auth_hash[uid], provider: "github")
+    if user
+      flash[:success] = "Logging in as returning user #{user.username}"
+    else
+      user = User.build_from_github(auth_hash)
+      if user.save
+        flash[:success] = "Logging in as new user #{user.username}"
+      else
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
+      end
+    end
+
+    session[:user_id] = user.id
+    return redirect_to root_path
   end
 
   # def login_form
