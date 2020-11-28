dd781fa68fd998b3084e257608a67fc09b61d3f1
Kareha Agesa <kmaagesa@gmail.com>
2020-11-27 18:23:02 -0800

    login validation with index/show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  before_action :require_login, only: [:index, :show]
 
   def render_404
     return render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
@@ -12,6 +13,14 @@ class ApplicationController < ActionController::Base
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def require_login
+    if find_user.nil?
+      flash[:status] = :failure
+      flash[:result_text] = "You must be logged in to view this"
+      redirect_to root_path
     end
   end
 end
