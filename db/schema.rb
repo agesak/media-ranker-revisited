1efba17f5403325cfdbc336e105f86e09d6f0b38
Kareha Agesa <kmaagesa@gmail.com>
2020-11-28 23:04:23 -0800

    Revert "got google oauth almost working"
    This reverts commit de0fc3f86e0f8286f24e90b4dbc4b26a28cb9dd7.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_082745) do
+ActiveRecord::Schema.define(version: 2020_11_27_224352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
