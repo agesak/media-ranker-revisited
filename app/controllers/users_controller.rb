6770bd5be134a8be07a7fa7da838b7bd2f7aeeb9
Kareha Agesa <kmaagesa@gmail.com>
2020-11-30 11:08:52 -0800

    delete comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,25 +38,7 @@ class UsersController < ApplicationController
           return redirect_to root_path
         end
       end
-
     end
-
-    # if auth_hash["provider"] == "github"
-    #   user = User.build_from_github(auth_hash)
-    # elsif auth_hash["provider"] == "google_oauth2"
-    #   user = User.build_from_google(auth_hash)
-    # end
-    #
-    # if user.save
-    #   flash[:status] = :success
-    #   flash[:result_text] = "Logged in as new user #{user.username}"
-    #   session[:user_id] = user.id
-    #   return redirect_to root_path
-    # else
-    #   flash[:status] = :failure
-    #   flash[:messages] = "Could not create new user account: #{user.errors.messages}"
-    #   return redirect_to root_path
-    # end
   end
 
   def logout
